4afaba25f4d31e7fa49dbc673b8a55d9946a97b4
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 14:57:43 -0600

    scrip

--- nave/base/script/script.js
+++ nave/base/script/script.js
@@ -1,10 +1,18 @@
 // Inicializar el mapa
 var mymap = L.map('mapid').setView([51.505, -0.09], 13); // coordenadas iniciales y nivel de zoom
 
-// Añadir el mapa base de OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(mymap);
+
+
+
+
+// Añadir el mapa base de OpenStreetMap
+
 
 // Añadir un marcador en una ubicación específica
 var marker = L.marker([51.5, -0.09]).addTo(mymap); // coordenadas del marcador
