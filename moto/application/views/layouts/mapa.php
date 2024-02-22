b6a99fa7d0a6744d91eff18a45d742a48c4870bf
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 17:34:41 -0600

    <b>Version</b> 1.2.0

--- moto/application/views/layouts/mapa.php
+++ moto/application/views/layouts/mapa.php
@@ -107,9 +107,9 @@
 
         <footer class="main-footer">
             <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.2.0
+                <b>Version</b> 1.2.0
             </div>
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+            <strong>Copyright &copy; 2024 <a href="https://sarkoceles.com.mx">sarkoceles</a>.</strong> All rights reserved.
         </footer>
 
         <!-- Control Sidebar -->
