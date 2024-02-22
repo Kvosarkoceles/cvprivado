a368b556f36b79a2ebaacf157e1e4d2318597100
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 15:27:42 -0600

    tutulo

--- nave/application/views/layouts/mapa.php
+++ nave/application/views/layouts/mapa.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moto</title>
+    <title>Mapa con Leaflet</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>nave/base/style/style.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>nave/base/leafletjs/leaflet.css">
 </head>
