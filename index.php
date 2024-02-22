8590d9e2f6d2f6b10edfa80d30c6989650b5212a
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 01:08:05 -0600

    index completo

--- index.php
+++ index.php
@@ -1,7 +1,91 @@
-<?php
-    // Incluir el pie de página
-    include '/plantillas/head.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>sarkoceles.com.mx</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <!-- Metadatos SEO -->
+    <meta
+      name="description"
+      content="Breve descripción de tu página que incluya palabras clave relevantes."
+    />
+    <meta
+      name="keywords"
+      content="Palabra clave 1, Palabra clave 2, Palabra clave 3"
+    />
+    <meta name="author" content="Tu Nombre" />
+
+    <!-- Metadatos sociales (opcional) -->
+    <meta property="og:title" content="Título de tu Página" />
+    <meta
+      property="og:description"
+      content="Breve descripción de tu página que incluya palabras clave relevantes."
+    />
+    <meta
+      property="og:image"
+      content="URL de una imagen representativa de tu página"
+    />
+    <meta property="og:url" content="URL completa de tu página" />
+    <meta name="twitter:card" content="summary_large_image" />
+
+    <link
+      href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="/assets/css/open-iconic-bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="/assets/css/animate.css"
+    />
+    <link
+      rel="stylesheet"
+      href="/assets/css/owl.carousel.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="/assets/css/owl.theme.default.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="/assets/css/magnific-popup.css"
+    />
+    <link rel="stylesheet" href="/assets/css/aos.css" />
+    <link
+      rel="stylesheet"
+      href="/assets/css/ionicons.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="/assets/css/flaticon.css"
+    />
+    <link
+      rel="stylesheet"
+      href="/assets/css/icomoon.css"
+    />
+    <link rel="stylesheet" href="/assets/css/style.css" />
+    
+    <!-- Google tag (gtag.js) -->
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-V68LV0H88W"
+    ></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "G-V68LV0H88W");
+    </script>
+  </head>
   <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
     <nav
       class="navbar navbar-expand-lg navbar-dark ftco_navbar ftco-navbar-light site-navbar-target"
@@ -48,7 +132,7 @@
               >
             </li>
             <li class="nav-item">
-              <a href="/fotos" class="nav-link"
+              <a href="/fotos.html" class="nav-link"
                 ><span>Fotografia</span></a
               >
             </li>
@@ -207,7 +291,192 @@
         </div>
       </div>
     </section>
-    <?php
-    // Incluir el pie de página
-    include '/plantillas/footer.php';
-?>
+    <footer class="ftco-footer ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Acerca de</h2>
+              <p>
+                La mayoría de los buenos programadores programan no porque
+                esperan que les pagen o que el público los adore, sino porque
+                programar es divertido.
+              </p>
+              <ul
+                class="ftco-footer-social list-unstyled float-md-left float-lft mt-5"
+              >
+                <li class="ftco-animate">
+                  <a href="#"><span class="icon-twitter"></span></a>
+                </li>
+                <li class="ftco-animate">
+                  <a href="#"><span class="icon-facebook"></span></a>
+                </li>
+                <li class="ftco-animate">
+                  <a href="#"><span class="icon-instagram"></span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">Menu</h2>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="/"
+                    ><span class="icon-long-arrow-right mr-2"></span>Inicio</a
+                  >
+                </li>
+                <li>
+                  <a href="/about"
+                    ><span class="icon-long-arrow-right mr-2"></span>Acerca
+                    de</a
+                  >
+                </li>
+                <li>
+                  <a href="/resume"
+                    ><span class="icon-long-arrow-right mr-2"></span
+                    >Experiencia</a
+                  >
+                </li>
+                <li>
+                  <a href="/services"
+                    ><span class="icon-long-arrow-right mr-2"></span
+                    >Servicios</a
+                  >
+                </li>
+                <li>
+                  <a href="/skills"
+                    ><span class="icon-long-arrow-right mr-2"></span
+                    >Habilidades</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Servicios</h2>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#"
+                    ><span class="icon-long-arrow-right mr-2"></span>Desarollo
+                    web</a
+                  >
+                </li>
+                <li>
+                  <a href="#"
+                    ><span class="icon-long-arrow-right mr-2"></span
+                    >Mantenimiento y reparacion</a
+                  >
+                </li>
+                <li>
+                  <a href="#"
+                    ><span class="icon-long-arrow-right mr-2"></span>Redes</a
+                  >
+                </li>
+                <li>
+                  <a href="#"
+                    ><span class="icon-long-arrow-right mr-2"></span
+                    >Mantenimiento</a
+                  >
+                </li>
+                <li>
+                  <a href="#"
+                    ><span class="icon-long-arrow-right mr-2"></span>CMS</a
+                  >
+                </li>
+                <li>
+                  <a href="#"
+                    ><span class="icon-long-arrow-right mr-2"></span>Hosting
+                    Web</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">¿Preguntas?</h2>
+              <div class="block-23 mb-3">
+                <ul>
+                  <li>
+                    <span class="icon icon-map-marker"></span
+                    ><span class="text"
+                      >Huayatla, Magdalena Contreras, Ciudad de México,
+                      MEX.</span
+                    >
+                  </li>
+                  <li>
+                    <a href="#"
+                      ><span class="icon icon-phone"></span
+                      ><span class="text">+52 56-1687-4064</span></a
+                    >
+                  </li>
+                  <li>
+                    <a href="#"
+                      ><span class="icon icon-envelope"></span>
+                      <font size="2">
+                        <span class="text">sarkoceles@ciencias.unam.mx</span>
+                      </font>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy;
+              <script>
+                document.write(new Date().getFullYear());
+              </script>
+              All rights reserved by
+              <a href="http://sarkoceles.mx" target="_blank">Sarkoceles</a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen">
+      <svg class="circular" width="48px" height="48px">
+        <circle
+          class="path-bg"
+          cx="24"
+          cy="24"
+          r="22"
+          fill="none"
+          stroke-width="4"
+          stroke="#eeeeee"
+        />
+        <circle
+          class="path"
+          cx="24"
+          cy="24"
+          r="22"
+          fill="none"
+          stroke-width="4"
+          stroke-miterlimit="10"
+          stroke="#F96D00"
+        />
+      </svg>
+    </div>
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="/assets/js/popper.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/jquery.easing.1.3.js"></script>
+    <script src="/assets/js/jquery.waypoints.min.js"></script>
+    <script src="/assets/js/jquery.stellar.min.js"></script>
+    <script src="/assets/js/owl.carousel.min.js"></script>
+    <script src="/assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="/assets/js/aos.js"></script>
+    <script src="/assets/js/jquery.animateNumber.min.js"></script>
+    <script src="/assets/js/scrollax.min.js"></script>
+    <script src="/assets/js/main.js"></script>
+  </body>
+</html>
