57cd68e09ffe151f7d456deecdba6c37d881243b
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 14:49:15 -0600

    estilos

--- nave/application/views/layouts/blank.php
+++ nave/application/views/layouts/blank.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mapa con Leaflet</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>nave/base/style/style.css">
 </head>
 <body>
     <div id="mapid"></div>
