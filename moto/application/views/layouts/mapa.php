e1e504be670c637e56d2fda3fa6b811abcb6e86c
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 15:49:04 -0600

    tamaño

--- moto/application/views/layouts/mapa.php
+++ moto/application/views/layouts/mapa.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Mapa con Leaflet</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>base/style/style.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>base/leafletjs/leaflet.css">
