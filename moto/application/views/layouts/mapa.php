1602e932c269b1312b5596a91b97672a47b427cc
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 16:36:38 -0600

    tamaño de mapa

--- moto/application/views/layouts/mapa.php
+++ moto/application/views/layouts/mapa.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-
     <title>Mapa</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>base/style/style.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>base/leafletjs/leaflet.css">
