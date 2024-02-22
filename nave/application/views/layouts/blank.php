46f88d13ff5eedf04ffb28252f1cd32a52719a1e
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 11:50:33 -0600

    map

--- nave/application/views/layouts/blank.php
+++ nave/application/views/layouts/blank.php
@@ -104,26 +104,7 @@
     <section class="content">
 
       <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-         
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
-        </div>
-        <div class="card-body">
-         aqui
-        </div>
-        <!-- /.card-body -->
-        
-        <!-- /.card-footer-->
-      </div>
+  mapa
       <!-- /.card -->
 
     </section>
