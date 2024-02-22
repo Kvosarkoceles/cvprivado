36e433587c0fade0e18bdc5ccf19698bbc6ea499
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 01:59:34 -0600

    gps

---
+++ gps/views/pagina/resume/index.php
@@ -0,0 +1,178 @@
+<section class="ftco-section ftco-no-pb" id="resume-section">
+      <div class="container">
+        <div class="row justify-content-center pb-5">
+          <div class="col-md-10 heading-section text-center ftco-animate">
+            <h1 class="big big-2">Experiencia</h1>
+            <h2 class="mb-4">Experiencia</h2>
+            <p>
+              El conocimiento se adquiere leyendo la letra pequeña de un
+              contrato; la experiencia, no leyéndola (Francis Bacon)
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="resume-wrap ftco-animate">
+              <span class="date">2022-2023</span>
+              <h2>Desarrollador Web Full Stack</h2>
+              <span class="position">Sti Solutions</span>
+              <p class="mt-4">
+                Encargado de la platafroma de rastreo y analisis de telimetrias,
+                desarrollo de microservicios para interacion a multiples
+                plataformas, desarrollo de bots de envio de alertas
+              </p>
+            </div>
+            <div class="resume-wrap ftco-animate">
+              <span class="date">2019-2020</span>
+              <h2>Soporte Técnico</h2>
+              <span class="position">VIOLETA RADIO XHCDMX 106.1 FM</span>
+              <p class="mt-4">
+                Soporte técnico al equipo de transmisión y administración del
+                servidor.
+              </p>
+            </div>
+            <div class="resume-wrap ftco-animate">
+              <span class="date">2019</span>
+              <h2>Instructor latex</h2>
+              <span class="position">Colectivo Ciencia y Sociedad</span>
+              <p class="mt-4">
+                Elaboración de textos científicos, así como la enseñar a
+                utilizar: caracteres especiales, acentos y ñ, estilo y tamaño de
+                los caracteres
+              </p>
+            </div>
+            <div class="resume-wrap ftco-animate">
+              <span class="date">2018</span>
+              <h2>Soporte Técnico</h2>
+              <span class="position">PRIME MOTOS SERVICE</span>
+              <p class="mt-4">
+                Soporte Tecnico a servidores, camaras ip y bases de datos de
+                almacen
+              </p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="resume-wrap ftco-animate">
+              <span class="date">2020-2022</span>
+              <h2>Encargado De Area Operativa</h2>
+              <span class="position">Grupo Digital Systems</span>
+              <p class="mt-4">
+                Elaboración de textos científicos, así como la enseñar a
+                utilizar
+              </p>
+            </div>
+            <div class="resume-wrap ftco-animate">
+              <span class="date">2016-2020</span>
+              <h2>Soporte Técnico Y Analista de datos</h2>
+              <span class="position"
+                >COMUNICACIÓN E INFORMACIÓN DE LA MUJER A.C.</span
+              >
+              <p class="mt-4">
+                Elaboración de textos científicos, así como la enseñar a
+                utilizar
+              </p>
+            </div>
+            <div class="resume-wrap ftco-animate">
+              <span class="date">2019</span>
+              <h2>Instructor Codeigniter 3</h2>
+              <span class="position">Colectivo Ciencia y Sociedad</span>
+              <p class="mt-4">
+                Durante este taller se desarrollo web, se realizo el sito
+                utilizando el framework Codeigniter 3
+                https://colectivocienciaysociedad.org
+              </p>
+            </div>
+            <div class="resume-wrap ftco-animate">
+              <span class="date">2007-2009</span>
+              <h2>Auxiliar Administrativo</h2>
+              <span class="position">Gobierno del Distrito Federal</span>
+              <p class="mt-4">
+                A small river named Duden flows by their place and supplies it
+                with the necessary regelialia. It is a paradisematic country, in
+                which roasted parts of sentences fly into your mouth.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center mt-5">
+          <div class="col-md-6 text-center ftco-animate">
+            <p>
+              <a
+                href="<?php echo base_url(); ?>assets/archivos/CVMiguelMendoza.pdf"
+                class="btn btn-primary py-4 px-5"
+                >Download CV</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section
+      class="ftco-section ftco-no-pt ftco-no-pb ftco-counter img"
+      id="section-counter"
+    >
+      <div class="container">
+        <div class="row d-md-flex align-items-center">
+          <div
+            class="col-md d-flex justify-content-center counter-wrap ftco-animate"
+          >
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="7638">0</strong>
+                <span>Rebanadas de pizza</span>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-md d-flex justify-content-center counter-wrap ftco-animate"
+          >
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="45678">0</strong>
+                <span>Kilometros en bicicleta</span>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-md d-flex justify-content-center counter-wrap ftco-animate"
+          >
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="6456">0</strong>
+                <span>Tazas de cafée</span>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-md d-flex justify-content-center counter-wrap ftco-animate"
+          >
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="254">0</strong>
+                <span>Clientes felices</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section
+      class="ftco-section ftco-hireme img margin-top"
+      style="
+        background-image: url(https://res.cloudinary.com/dqmck2ujs/image/upload/v1588488686/curriculum/49849180657_b634789da5_o_ywnldv.jpg);
+      "
+    >
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-7 ftco-animate text-center">
+            <h2>Estoy <span>disponible </span> para trabajar</h2>
+            <p>
+              Lo bello del desierto es que en algún lugar esconde un pozo. "El
+              principito" (1943), Antoine De Saint-Exupéry
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
---
+++ gps/views/pagina/services/index.php
@@ -0,0 +1,79 @@
+<section class="ftco-section" id="services-section">
+	<div class="container">
+		<div class="row justify-content-center py-5 mt-5">
+			<div class="col-md-12 heading-section text-center ftco-animate">
+				<h1 class="big big-2">Servicios</h1>
+				<h2 class="mb-4">Servicios</h2>
+				<p>Cuanto más espere el cliente, más dificil es producir excelente servicio.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4 text-center d-flex ftco-animate">
+				<a href="" class="services-1">
+					<span class="icon">
+						<i class="flaticon-analysis"></i>
+					</span>
+					<div class="desc">
+						<h3 class="mb-5">Desarollo web</h3>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4 text-center d-flex ftco-animate">
+				<a href="" class="services-1">
+					<span class="icon">
+						<i class="flaticon-flasks"></i>
+					</span>
+					<div class="desc">
+						<h3 class="mb-5">Mantenimiento y reparación</h3>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4 text-center d-flex ftco-animate">
+				<a href="#" class="services-1">
+					<span class="icon">
+						<i class="flaticon-ideas"></i>
+					</span>
+					<div class="desc">
+						<h3 class="mb-5">Redes</h3>
+					</div>
+				</a>
+			</div>
+
+			<div class="col-md-4 text-center d-flex ftco-animate">
+				<a href="#" class="services-1">
+					<span class="icon">
+						<i class="flaticon-analysis"></i>
+					</span>
+					<div class="desc">
+						<h3 class="mb-5">Soporte Remoto</h3>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4 text-center d-flex ftco-animate">
+				<a href="#" class="services-1">
+					<span class="icon">
+						<i class="flaticon-flasks"></i>
+					</span>
+					<div class="desc">
+						<h3 class="mb-5">CMS</h3>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4 text-center d-flex ftco-animate">
+				<a href="#" class="services-1">
+					<span class="icon">
+						<i class="flaticon-ideas"></i>
+					</span>
+					<div class="desc">
+						<h3 class="mb-5">Diseño</h3>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+
+
