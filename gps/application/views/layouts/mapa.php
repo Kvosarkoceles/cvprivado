5e851ae1d44dbc90fd280ea270be76b5780aa4d1
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 21:01:57 -0600

    url

--- gps/application/views/layouts/mapa.php
+++ gps/application/views/layouts/mapa.php
@@ -50,8 +50,8 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="<?php echo base_url(); ?>/base/index3.html" class="brand-link">
-                <img src="<?php echo base_url(); ?>/base/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <a href="<?php echo base_url(); ?>base/index3.html" class="brand-link">
+                <img src="<?php echo base_url(); ?>base/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">MOTO-GPS</span>
             </a>
 
