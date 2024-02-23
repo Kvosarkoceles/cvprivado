5993cbcd2ac244cd0ed84c2c1570b59922b644db
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-23 09:26:29 -0600

    nav

--- gps/application/views/layouts/celular.php
+++ gps/application/views/layouts/celular.php
@@ -35,9 +35,7 @@
             </ul>
 
             <!-- Right navbar links -->
-            <ul class="navbar-nav ml-auto">
-
-            </ul>
+           
         </nav>
         <!-- /.navbar -->
 
