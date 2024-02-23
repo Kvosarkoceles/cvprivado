a3d9c87f3db3a0ff3a75de32322077ffe0430296
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 20:20:00 -0600

    menu

--- moto/application/views/layouts/mapa.php
+++ moto/application/views/layouts/mapa.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mapa con Leaflet</title>
+    <title>MOTO-GPS</title>
 
-<!-- 
+    <!-- 
     <link rel="stylesheet" href="<?php echo base_url(); ?>base/style/style.css"> -->
 
 
@@ -33,10 +33,10 @@
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="<?php echo base_url(); ?>/base/index3.html" class="nav-link">Home</a>
+                    <a href="<?php echo base_url(); ?>" class="nav-link">Home</a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="#" class="nav-link">Contact</a>
+                    <a href="../" class="nav-link">Contact</a>
                 </li>
             </ul>
 
@@ -52,7 +52,7 @@
             <!-- Brand Logo -->
             <a href="<?php echo base_url(); ?>/base/index3.html" class="brand-link">
                 <img src="<?php echo base_url(); ?>/base/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">MOTOG-GPS</span>
+                <span class="brand-text font-weight-light">MOTO-GPS</span>
             </a>
 
             <!-- Sidebar -->
@@ -60,38 +60,26 @@
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                        <li class="nav-header">EXAMPLES</li>
+                        <li class="nav-header">Veiculos</li>
                         <li class="nav-item">
-                            <a href="../gallery.html" class="nav-link">
-                                <i class="nav-icon far fa-image"></i>
-                                <p>
-                                    Gallery
-                                </p>
+                           
+                     
+
+                        <li class="nav-item">
+                            <a href="../search/simple.html" class="nav-link">
+                                <i class="fa-solid fa-motorcycle"></i>
+                                <p> Moto</p>
                             </a>
                         </li>
+
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-search"></i>
-                                <p>
-                                    Search
-                                    <i class="fas fa-angle-left right"></i>
-                                </p>
+                            <a href="../search/simple.html" class="nav-link">
+                                <i class="fa-solid fa-mobile-retro"></i>
+                                <p> Celular</p>
                             </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="../search/simple.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Simple Search</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="../search/enhanced.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Enhanced</p>
-                                    </a>
-                                </li>
-                            </ul>
                         </li>
+
+
                     </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
@@ -101,7 +89,7 @@
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper" id="mapid">
-            
+
         </div>
         <!-- /.content-wrapper -->
 
