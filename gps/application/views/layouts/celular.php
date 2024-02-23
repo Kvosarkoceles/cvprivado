aa66635fa87ff81a2e76fb57bee49ba060a95962
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-23 09:28:24 -0600

    lista

--- gps/application/views/layouts/celular.php
+++ gps/application/views/layouts/celular.php
@@ -26,17 +26,17 @@
     <!-- Site wrapper -->
     <div class="wrapper">
         <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      
             <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
+        
+        
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>              
-            </ul>
+               
+         
 
             <!-- Right navbar links -->
            
-        </nav>
+
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
@@ -55,7 +55,9 @@
                         <li class="nav-header">Veiculos</li>
                         <li class="nav-item">
                            
-                     
+                     <li class="nav-item">            
+                         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                    </li>
 
                         <li class="nav-item">
                             <a href="<?php echo base_url(); ?>moto" class="nav-link">
