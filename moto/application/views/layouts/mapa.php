ed814836a2fe709f19ad155508dba18456856429
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-22 18:49:35 -0600

    moto

--- moto/application/views/layouts/mapa.php
+++ moto/application/views/layouts/mapa.php
@@ -52,7 +52,7 @@
             <!-- Brand Logo -->
             <a href="<?php echo base_url(); ?>/base/index3.html" class="brand-link">
                 <img src="<?php echo base_url(); ?>/base/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">MOTOG-GPS</span>
             </a>
 
             <!-- Sidebar -->
