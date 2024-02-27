960b528810b8bb5764d7257b311298476ce5baf3
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-27 15:22:56 -0600

    response

--- gps/base/script/moto.js
+++ gps/base/script/moto.js
@@ -87,7 +87,8 @@ async function informacion() {
 var ignicionElement = $('.col-xs-6 label.ignicion + span');
 
 // Obtener el texto dentro del elemento encontrado
-var estadoIgnicion = response.trim();
+console.log(typeof response);
+var estadoIgnicion = response;
 
 // Imprimir el estado de la ignición
 console.log("Estado de la ignición:", estadoIgnicion);
