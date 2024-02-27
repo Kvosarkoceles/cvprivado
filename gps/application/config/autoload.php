1f23e0ef55b1b3f51221c077c4f9537f84632661
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-26 23:13:52 -0600

    auto libre

--- gps/application/config/autoload.php
+++ gps/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('output');
+$autoload['libraries'] = array();
 
 /*
 | -------------------------------------------------------------------
