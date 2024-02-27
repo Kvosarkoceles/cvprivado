86264cebe76fa826c845aeef05268c8333d7916c
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-27 16:00:22 -0600

    deleted

--- gps/base/script/moto.js
+++ gps/base/script/moto.js
@@ -108,7 +108,7 @@ async function informacion() {
       // Crear un elemento jQuery a partir del HTML recibido
       var $html = $(jsonString);
 
-      console.log('$html ' ,$html.0.outerText);
+      console.log('$html ' ,$html);
       var keys = Object.keys($html);
 
 // Imprimir las llaves
