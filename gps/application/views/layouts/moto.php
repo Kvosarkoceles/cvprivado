11937cf212690b77d399d2ca6a020d08230c9a68
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-28 17:07:33 -0600

    eventos

--- gps/application/views/layouts/moto.php
+++ gps/application/views/layouts/moto.php
@@ -39,10 +39,7 @@
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
                     <a href="<?php echo base_url(); ?>moto/posiciones" class="nav-link">Posiciones</a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="<?php echo base_url(); ?>moto/eventos" class="nav-link">Eventos</a>
-                </li>
+                </li>              
                 <li class="nav-item d-none d-sm-inline-block">
                     <a href="<?php echo base_url(); ?>moto/viajes" class="nav-link">Viajes</a>
                 </l
