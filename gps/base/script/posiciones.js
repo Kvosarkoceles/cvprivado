934170adb247d980c1605c258773ea090eca79f8
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-28 00:07:37 -0600

    add

--- gps/base/script/posiciones.js
+++ gps/base/script/posiciones.js
@@ -57,11 +57,11 @@ async function viajes() {
         //   coordinates.push(posicion);
         // }
 var isInArrat =  $.inArray(coords, coordinates);
-// console.log(isInArrat);
-        if (isInArrat === 0) {
+console.log(isInArrat);
+        if (isInArrat === -1) {
           coordinates.push(coords);
           posicionArray.push(posicion);
-          // console.log(posicion);
+          console.log(posicion);
         }
       });
 
