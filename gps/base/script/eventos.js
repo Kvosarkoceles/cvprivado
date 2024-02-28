21f2a645eb528dfe0d6d119f82ee46740f41995b
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-28 14:42:36 -0600

    date

--- gps/base/script/eventos.js
+++ gps/base/script/eventos.js
@@ -17,8 +17,8 @@ async function viajes() {
   // console.log("informacion funcion" + velocidad);
   var data = {
     ID_disp: 1970000012,
-    f1: "2024-02-15 00:00:00",
-    f2: "2024-02-28 18:26:21",
+    f1: "2024-02-25 19:03:40",
+    f2: "2024-02-25 19:37:33",
     dbip: "imovit.cx0btphnat72.us-east-1.rds.amazonaws.com",
     db: "awsdev",
     lgw_id: 133,
