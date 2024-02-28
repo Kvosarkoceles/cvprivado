ce54da27a60e3f5259924a038d21e57b9714a771
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-28 17:40:34 -0600

    nav

--- gps/application/views/layouts/moto.php
+++ gps/application/views/layouts/moto.php
@@ -62,6 +62,7 @@
 
             <!-- Sidebar -->
             <div class="sidebar">
+                aqui va la info del resumen
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
