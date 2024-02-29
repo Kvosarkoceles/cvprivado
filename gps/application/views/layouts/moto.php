d368d9b89483231bd9ea046e3be9d5501e0cf804
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-28 18:28:27 -0600

    mapas

--- gps/application/views/layouts/moto.php
+++ gps/application/views/layouts/moto.php
@@ -130,7 +130,7 @@
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                        <li class="nav-header">Veiculos</li>
+                        <li class="nav-header">Mapas</li>
                         <li class="nav-item">
 
 
