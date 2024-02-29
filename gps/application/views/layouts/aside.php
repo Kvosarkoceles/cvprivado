67dee5bfce90767b345db2f659036dda4cc66a33
sarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-29 13:34:38 -0600

    posicionesTabla

--- gps/application/views/layouts/aside.php
+++ gps/application/views/layouts/aside.php
@@ -68,6 +68,98 @@
                 </table>
             </div>
         </div>
+        <div id="posicionesTabla">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Posiciones</h3>
+                </div>
+                <!-- ./card-header -->
+                <div class="card-body p-0">
+                    <table class="table table-hover">
+                        <tbody>
+                            <tr>
+                                <td class="border-0">183</td>
+                            </tr>
+                            <tr data-widget="expandable-table" aria-expanded="true">
+                                <td>
+                                    <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
+                                    219
+                                </td>
+                            </tr>
+                            <tr class="expandable-body">
+                                <td>
+                                    <div class="p-0" style="">
+                                        <table class="table table-hover">
+                                            <tbody>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>
+                                                        <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
+                                                        219-1
+                                                    </td>
+                                                </tr>
+                                                <tr class="expandable-body d-none">
+                                                    <td>
+                                                        <div class="p-0" style="display: none;">
+                                                            <table class="table table-hover">
+                                                                <tbody>
+                                                                    <tr>
+                                                                        <td>219-1-1</td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>219-1-2</td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>219-1-3</td>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="true">
+                                                    <td>
+                                                        <button type="button" class="btn btn-primary p-0">
+                                                            <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
+                                                        </button>
+                                                        219-2
+                                                    </td>
+                                                </tr>
+                                                <tr class="expandable-body">
+                                                    <td>
+                                                        <div class="p-0" style="">
+                                                            <table class="table table-hover">
+                                                                <tbody>
+                                                                    <tr>
+                                                                        <td>219-2-1</td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>219-2-2</td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>219-2-3</td>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>219-3</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>657</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div>
         <div id="boton_menu">
             <div style='margin-bottom: 5px; color: white;' id="boton">
                 <button onclick='verUbicacion()' id="myUbicacion" type="button" class="btn btn-block btn-primary" style="display: none;">Localizar</button>
