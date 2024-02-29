a2386c39b6940b8d452a73656afe0fcd009234b6
sarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-29 13:55:55 -0600

    tabla

--- gps/application/views/layouts/aside.php
+++ gps/application/views/layouts/aside.php
@@ -77,13 +77,10 @@
                 <div class="card-body p-0">
                     <table class="table table-hover">
                         <tbody>
-                            <tr>
-                                <td class="border-0">183</td>
-                            </tr>
-                            <tr data-widget="expandable-table" aria-expanded="true">
+                            <tr data-widget="expandable-table" aria-expanded="false">
                                 <td>
                                     <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
-                                    219
+                                    10:48:07
                                 </td>
                             </tr>
                             <tr class="expandable-body">
@@ -93,67 +90,229 @@
                                             <tbody>
                                                 <tr data-widget="expandable-table" aria-expanded="false">
                                                     <td>
-                                                        <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
-                                                        219-1
+                                                     
+                                                        Lat:
                                                     </td>
+                                                    <td>898908098</td>
                                                 </tr>
-                                                <tr class="expandable-body d-none">
-                                                    <td>
-                                                        <div class="p-0" style="display: none;">
-                                                            <table class="table table-hover">
-                                                                <tbody>
-                                                                    <tr>
-                                                                        <td>219-1-1</td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td>219-1-2</td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td>219-1-3</td>
-                                                                    </tr>
-                                                                </tbody>
-                                                            </table>
-                                                        </div>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
                                                     </td>
+                                                    <td>898908098</td>
                                                 </tr>
-                                                <tr data-widget="expandable-table" aria-expanded="true">
-                                                    <td>
-                                                        <button type="button" class="btn btn-primary p-0">
-                                                            <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
-                                                        </button>
-                                                        219-2
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
                                                     </td>
+                                                    <td>898908098</td>
                                                 </tr>
-                                                <tr class="expandable-body">
-                                                    <td>
-                                                        <div class="p-0" style="">
-                                                            <table class="table table-hover">
-                                                                <tbody>
-                                                                    <tr>
-                                                                        <td>219-2-1</td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td>219-2-2</td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td>219-2-3</td>
-                                                                    </tr>
-                                                                </tbody>
-                                                            </table>
-                                                        </div>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
                                                     </td>
+                                                    <td>898908098</td>
                                                 </tr>
-                                                <tr>
-                                                    <td>219-3</td>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
                                                 </tr>
                                             </tbody>
                                         </table>
                                     </div>
                                 </td>
                             </tr>
-                            <tr>
-                                <td>657</td>
+                            <tr data-widget="expandable-table" aria-expanded="false">
+                                <td>
+                                    <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
+                                    10:48:07
+                                </td>
                             </tr>
+                            <tr class="expandable-body">
+                                <td>
+                                    <div class="p-0" style="">
+                                        <table class="table table-hover">
+                                            <tbody>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>
+                                                     
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr data-widget="expandable-table" aria-expanded="false">
+                                <td>
+                                    <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
+                                    10:48:07
+                                </td>
+                            </tr>
+                            <tr class="expandable-body">
+                                <td>
+                                    <div class="p-0" style="">
+                                        <table class="table table-hover">
+                                            <tbody>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>
+                                                     
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr data-widget="expandable-table" aria-expanded="false">
+                                <td>
+                                    <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
+                                    10:48:07
+                                </td>
+                            </tr>
+                            <tr class="expandable-body">
+                                <td>
+                                    <div class="p-0" style="">
+                                        <table class="table table-hover">
+                                            <tbody>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>
+                                                     
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr data-widget="expandable-table" aria-expanded="false">
+                                <td>
+                                    <i class="expandable-table-caret fas fa-caret-right fa-fw"></i>
+                                    10:48:07
+                                </td>
+                            </tr>
+                            <tr class="expandable-body">
+                                <td>
+                                    <div class="p-0" style="">
+                                        <table class="table table-hover">
+                                            <tbody>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>
+                                                     
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                                <tr data-widget="expandable-table" aria-expanded="false">
+                                                    <td>                                                      
+                                                        Lat:
+                                                    </td>
+                                                    <td>898908098</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </td>
+                            </tr>
+
                         </tbody>
                     </table>
                 </div>
