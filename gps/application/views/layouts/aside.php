1110e996ee8c401eaa6425cc0a81c0513fc3d812
sarkoceles <sarkoceles@ciencias.unam.mx>
2024-03-07 14:07:56 -0600

    start

--- gps/application/views/layouts/aside.php
+++ gps/application/views/layouts/aside.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="javascript:limpiarTabla()" class="brand-link">
+    <a href="javascript:start()" class="brand-link">
         <img src="<?php echo base_url(); ?>base/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">MOTO-GPS</span>
     </a>
