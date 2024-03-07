527a3f357cc97c1c61a54e80d131c3f2c326b66d
sarkoceles <sarkoceles@ciencias.unam.mx>
2024-03-07 14:03:47 -0600

    sarkoceles

--- gps/application/views/layouts/footer.php
+++ gps/application/views/layouts/footer.php
@@ -4,7 +4,7 @@
       <div class="float-right d-none d-sm-block">
         <b>Version</b> 3.3.0
       </div>
-      <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+      <strong>Copyright &copy; 2024 <a href="https://sarkoceles.com.mx">Sarkoceles</a>.</strong> All rights reserved.
     </footer>
 
     <!-- Control Sidebar -->
