8c4de841beff76428de8e3eb1b1f991cc5880766
sarkoceles <sarkoceles@ciencias.unam.mx>
2024-03-23 23:11:42 -0600

    url

--- gps/application/config/config.php
+++ gps/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own. 
 |
 */
-$config['base_url'] = 'https://sarkoceles.com.mx/gps/';
+$config['base_url'] = 'https://192.168.100.9/cvprivado/gps/';
 
 /*
 |-------------------------------------------------------------------------
